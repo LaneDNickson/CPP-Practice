7d41763c89d15b76f4bc77b41fb15c79dad7e2d2
Lane Nickson <lanednickson@gmail.com>
2022-12-29 12:06:26 -0500

    Working on practice

---
+++ exercises/ex1/ex1.cpp
@@ -0,0 +1,16 @@
+/**
+ * Exercise 1
+ *
+ * Write a program in C++ to print a welcome text in a separate line.
+ * 
+ * @author Lane Nickson
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Welcome to my exercise 1 program!\n";
+    cout << "---------------------------------\n";
+    cout << "Have a nice day!\n" << endl;
+}
---
+++ exercises/ex2/ex2.cpp
@@ -0,0 +1,18 @@
+/**
+ * Exercise 2
+ *
+ * Write a program in C++ to print the sum of two numbers.
+ * 
+ * @author Lane Nickson
+ */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1 = 29;
+    int num2 = 30;
+    cout << "Print the sum of two numbers: \n";
+    cout << "---------------------------------\n";
+    cout << "The sum of "<< num1 << " and " << num2 <<" is: " << num1 + num2 << endl;
+}
